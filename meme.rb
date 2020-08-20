df4fd7478bbf058075533fc0f028905eab30b1de
Dominique Doux <d.doux@criteo.com>
2020-08-20 18:29:30 +0200

    Add timezone.rb file & improve bot response
    Bot response:
    - add !time and !man
    - delete the message posted in the channel

--- meme.rb
+++ meme.rb
@@ -3,14 +3,43 @@
 
 require 'discordrb'
 require './weather.rb'
+require './timezone.rb'
 
 token = ENV['DISCORD_TOKEN']
 
 # Instantiate the Mémé
 meme = Discordrb::Bot.new token: "#{token}"
-  
+
+meme.message(content: '!man') { |event|
+  event.user.pm("Comment Mémé fonctionne: \
+    \n!man: affiche cette aide
+    \n!hello: répond hello
+    \n!time [ville]: affiche l'heure local dans la ville spécifiée \
+    \n!weather [ville]: affiche la météo dans la ville spécifiée")
+  event.message.delete
+}
+
 meme.message(content: '!hello' ) { |event|
-  event.user.pm("Hello #{event.user.name}")
+  event.user.pm("Hello #{event.user.name} !")
+  event.message.delete
+}
+
+meme.message(content: '!time') { |event|
+  event.user.pm("How !time command works !time: \
+    \nFor now you can only have the time for Paris and Papeete \
+    \nE.g: !time paris")
+}
+
+meme.message(content: '!time paris') { |event|
+  location_time = what_time('paris')
+  event.user.pm("Il est #{location_time} à Paris")
+  event.message.delete
+}
+
+meme.message(content: '!time papeete') { |event|
+  location_time = what_time('papeete')
+  event.user.pm("Il est #{location_time} à Papeete")
+  event.message.delete
 }
 
 meme.message(content: '!weather') { |event|
@@ -18,8 +47,7 @@ meme.message(content: '!weather') { |event|
   event.user.pm("#{city_weather['name']}, #{city_weather['weather'][0]['description']}\
     \nMin: #{city_weather['main']['temp_min']}°, Max: #{city_weather['main']['temp_max']}°\
     \nHumidité: #{city_weather['main']['humidity']}%, Vent: #{((city_weather['wind']['speed'] / 1000) * 3600).round}Km/h")
+  event.message.delete
 }
 
-
 meme.run
-
---
+++ timezone.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def what_time(where)
+  case where
+  when 'paris'
+    Time.now.utc.localtime("+02:00").strftime("%H:%M")
+  when 'papeete'
+    Time.now.utc.localtime("-10:00").strftime("%H:%M")
+  else
+    "Mémé doesn't know this city !"
+  end
+end
+
+puts what_time('paris')
