768704137ebd688324ab00308e14b64f7604a78a
Dominique Doux <d.doux@criteo.com>
2020-08-25 16:54:02 +0200

    Add new city in timezone.rb

--- timezone.rb
+++ timezone.rb
@@ -9,6 +9,24 @@ def what_time(where)
   when 'papeete'
     t = Time.now.utc.localtime("-10:00").strftime("%H:%M")
     "il est #{t} à Papeete"
+  when 'londres'
+    t = Time.now.utc.localtime("+01:00").strftime("%H:%M")
+    "il est #{t} à Londres"
+  when 'new-york'
+    t = Time.now.utc.localtime("-04:00").strftime("%H:%M")
+    "il est #{t} à New York"
+  when 'tokyo'
+    t = Time.now.utc.localtime("+09:00").strftime("%H:%M")
+    "il est #{t} à Tokyo"
+  when 'san-fransisco'
+    t = Time.now.utc.localtime("-07:00").strftime("%H:%M")
+    "il est #{t} à San Fransisco"
+  when 'moscou'
+    t = Time.now.utc.localtime("+03:00").strftime("%H:%M")
+    "il est #{t} à Moscou"
+  when 'berlin'
+    t = Time.now.utc.localtime("+02:00").strftime("%H:%M")
+    "il est #{t} à Berlin"
   else
     "Mémé doesn't know this city !"
   end
