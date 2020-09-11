1c214e2f31727339dfb50278eec9f9d8da1dad0d
dominik978 <dominik978@gmail.com>
2020-09-11 22:28:03 +0200

    Change response format in timezone.rb

--- timezone.rb
+++ timezone.rb
@@ -5,29 +5,29 @@ def what_time(where)
   case where
   when 'paris'
     t = Time.now.utc.localtime("+02:00").strftime("%H:%M")
-    "il est #{t} à Paris"
+    "Il est #{t} à Paris"
   when 'papeete'
     t = Time.now.utc.localtime("-10:00").strftime("%H:%M")
-    "il est #{t} à Papeete"
+    "Il est #{t} à Papeete"
   when 'londres'
     t = Time.now.utc.localtime("+01:00").strftime("%H:%M")
-    "il est #{t} à Londres"
+    "Il est #{t} à Londres"
   when 'new-york'
     t = Time.now.utc.localtime("-04:00").strftime("%H:%M")
-    "il est #{t} à New York"
+    "Il est #{t} à New York"
   when 'tokyo'
     t = Time.now.utc.localtime("+09:00").strftime("%H:%M")
-    "il est #{t} à Tokyo"
+    "Il est #{t} à Tokyo"
   when 'san-fransisco'
     t = Time.now.utc.localtime("-07:00").strftime("%H:%M")
-    "il est #{t} à San Fransisco"
+    "Il est #{t} à San Fransisco"
   when 'moscou'
     t = Time.now.utc.localtime("+03:00").strftime("%H:%M")
-    "il est #{t} à Moscou"
+    "Il est #{t} à Moscou"
   when 'berlin'
     t = Time.now.utc.localtime("+02:00").strftime("%H:%M")
-    "il est #{t} à Berlin"
+    "Il est #{t} à Berlin"
   else
-    "Mémé doesn't know this city !"
+    "Mémé ne connait pas cette ville !"
   end
 end
